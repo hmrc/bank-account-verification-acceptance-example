84880e2d7419760c086bb2b224bf4ccbdc437cd5
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2022-06-27 10:45:36 +0100

    #NOJIRA Dependency updates
    Add in explicit dependency on okhttp (previously transitive)
    Move WebDriverWait initialisation over to the Selenium 4 standard

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,7 @@ object Dependencies {
     "org.scalatestplus"   %% "selenium-3-141"                     % "3.2.10.0" % Test,
     "com.vladsch.flexmark" % "flexmark-all"                       % "0.62.2"   % Test,
     "uk.gov.hmrc"         %% "webdriver-factory"                  % "0.+"      % Test,
+    "com.squareup.okhttp3" % "okhttp"                             % "4.10.0"   % Test,
     "org.mock-server"      % "mockserver-netty"                   % "5.12.0"   % Test,
     "org.assertj"          % "assertj-core"                       % "3.22.0"   % Test,
     "uk.gov.hmrc"         %% "bank-account-verification-frontend" % "0.+"      % Test,
